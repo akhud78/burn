477933131751c378f304b6e280f3826d252153f1
akhud78 <akhudiakov@gmail.com>
2024-07-03 10:03:12 +0300

    but

---
+++ burn.bat
@@ -0,0 +1,6 @@
+@echo OFF
+rem burn.bat COM3
+set com=%1
+set "soc=gk7205v200"
+set "adr=0x42000000"
+python burn --chip %soc% --file=u-boot/u-boot-%soc%-universal.bin -p %com% --break && echo fatload mmc 0:1 %adr% %soc%.scr; source %adr% | plink -serial %com% -sercfg 115200,8,n,1,N
